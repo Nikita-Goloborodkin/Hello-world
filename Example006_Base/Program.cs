8ff6fc0f0278061b56689bf9d7d7439a360e56fe
Nikita <goloborodkin.nikita2018@gmail.com>
2023-02-10 23:50:17 +0300

    Search for max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = -1;
+int b = 8;
+int c = 10;
+int d = 23;
+int e = 13;
+
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
