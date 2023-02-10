70c041c2837b52df976dec06083167225f70213e
Nikita <goloborodkin.nikita2018@gmail.com>
2023-02-10 23:41:05 +0300

    Example005_if_or_else+.ToLower()

---
+++ Example005_if_or_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите своё имя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "George")
+{
+    Console.Write("Yeah, it is George");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
